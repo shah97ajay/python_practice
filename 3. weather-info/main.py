e32c8ca2ff6e5562e7747ec722dc5cf9bf35b45a
AJAY KUMAR <shah97ajay@gmail.com>
2021-04-30 20:38:46 +0530

    Update main.py

--- 3. weather-info/main.py
+++ 3. weather-info/main.py
@@ -5,3 +5,6 @@ city = input("enter a city: ")
 base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+api_key+"&q="+city
 weather_data = requests.get(base_url).json()
 pprint(weather_data)
+a = input('\npress enter to exit')
+
+
