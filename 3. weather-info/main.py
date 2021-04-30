d809ceaf6970fa5788f43d6e9abf3cf8c6f5dbc0
AJAY KUMAR <shah97ajay@gmail.com>
2021-04-30 20:40:52 +0530

    Update main.py

--- 3. weather-info/main.py
+++ 3. weather-info/main.py
@@ -1,10 +1,10 @@
 import requests
 from pprint import pprint
-api_key = 'b69dffbeebf9d1827f468302f7c76eb4'
+api_key = '' # enter api key you will find from openweathermap.org
 city = input("enter a city: ")
 base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+api_key+"&q="+city
 weather_data = requests.get(base_url).json()
 pprint(weather_data)
-a = input('\npress enter to exit')
+
 
 
